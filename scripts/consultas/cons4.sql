ae6dd8273251815016b474a235d74ec5891e2331
Antonio <antonio.michelineto@gmail.com>
2025-06-24 23:44:55 -0300

    mudanças

--- scripts/consultas/cons4.sql
+++ scripts/consultas/cons4.sql
@@ -1,8 +1,9 @@
--- Consulta 4: Lista alunos de Ciência da Computação na disciplina de Banco de Dados.
+-- Consulta 4: Lista 5 alunos de Ciência da Computação na disciplina de Banco de Dados.
 SELECT
     u.Nome || ' ' || u.Sobrenome AS "Nome do Aluno",
-    c.nome_Curso AS "Curso do Aluno"
-FROM
+    c.nome_Curso AS "Curso do Aluno",
+    u.ID_usuario AS "ID"
+FROM 
     Usuario u
 JOIN
     Matricula m ON u.ID_usuario = m.ID_aluno
@@ -15,4 +16,5 @@ JOIN -- Join para garantir que o aluno é do curso de Ciência da Computação
 JOIN
     Curso c ON ee.ID_curso = c.ID_Curso AND c.nome_Curso = 'Ciência da Computação'
 WHERE
-    d.Nome_Disciplina = 'Banco de Dados' AND m.Status = 'Ativa';
+    d.Nome_Disciplina = 'Banco de Dados' AND m.Status = 'Ativa'
+LIMIT 5;
